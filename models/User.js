c4c33a2f35fb30a0be2f21894e2c3289b4d5029c
Samuel Early <samleeearly@gmail.com>
2019-07-26 14:12:58 -0700

    edit/update ctrl, routes and edit page

--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: String,
+    name: String,
     email: String,
+    phone: String,
     password: String,
     pets: [{
         type: mongoose.Schema.Types.ObjectId,
