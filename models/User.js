0c0e7abbe8fbaa2dcca5cbc1e84da588ada385da
Samuel Early <samuelearly@Samuels-MacBook.local>
2019-07-26 10:58:03 -0700

    set up file structure for users & user model

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    pets: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Dogs'
+    }]
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
