d51012264e7d7df7efecaf8e70903730d3b65811
Olvin <olvin.bolanos@yahoo.com>
2019-07-26 13:03:47 -0700

    made the index, controller, and router for dog

---
+++ controllers/dogControllers.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const Dog = require('../models/dog')
+
+const dogController = {
+    find: async (req, res) => {
+        try {
+            const Doggy = await Dog.find({})
+    
+            res.render('dog/dog.ejs', {
+                dogs : Doggy
+            })
+    
+        } catch (err) {
+            res.send(err)
+        }
+    },
+    
+} 
+
+module.exports = ;
