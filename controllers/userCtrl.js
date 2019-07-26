d33947006d23d46ab2fb931c327b85498d11c1a9
Samuel Early <samleeearly@gmail.com>
2019-07-26 11:25:07 -0700

    set up user controller

---
+++ controllers/userCtrl.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const Pet = require('../models/dog');
+
+const userController = {
+    
+}
+
+module.exports = userController;
--- models/User.js
+++ models/User.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: String,
+    phone: String,
+    email: String,
     pets: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Dogs'
