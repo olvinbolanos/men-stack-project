8a13543c81742f67206ff9b86a2cc4f3f4f00dc8
Samuel Early <samleeearly@gmail.com>
2019-07-26 11:37:13 -0700

    created user index

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/authors');
 
-
+router.get('/', userController.index);
 
 
 module.exports = router;
