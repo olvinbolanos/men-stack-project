8f57bcb12ca72919bafff99bed39019753a50f53
Samuel Early <samuelearly@Samuels-MacBook.local>
2019-07-26 11:37:13 -0700

    created user index

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -3,7 +3,16 @@ const User = require('../models/user');
 const Pet = require('../models/dog');
 
 const userController = {
-    
+    index: async (req, res) => {
+        try{
+            const foundUsers = await User.find({})
+            res.render('users/index.ejs', {
+                user: foundUsers
+            })
+        }catch (err){
+            res.send(err)
+        }
+    }
 }
 
 module.exports = userController;
