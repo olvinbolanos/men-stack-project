74ffc466c175311d2890a95c43aea97aaa536082
David Zinser <dmzinser@gmail.com>
2019-07-26 16:04:50 -0700

    Create Login & Register Session Info w/Encryption & Linkages

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 const authController = {
   loginPage: async (req, res) => {
@@ -15,18 +16,43 @@ const authController = {
     } catch(err) {
       res.send(err);
     }
+  },
+  submitLoginInfo: async (req, res) => {
+    try {
+      const foundUser = await User.findOne({username: req.body.username});
+      if(foundUser){
+        if(bcrypt.compareSync(req.body.password, foundUser.password)){
+          req.session.username = foundUser.username;
+          req.session.logged = true;
+          req.session.message = 'Welcome Back!';
+          res.redirect('/dogs');
+        } else {
+          req.sessions.message = 'Incorrect Username or Password'
+          res.redirect('/');
+        }
+      } else {
+        req.session.message = 'Incorrect Username or Password'
+        res.redirect('/');
+      }
+    } catch(err) {
+      res.send(err);
+    }
+  },
+  submitNewAccountInfo: async (req, res) => {
+    const password = req.body.password;
+    const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    req.body.password = hashedPassword;
+    try {
+      const createdUser = await User.create(req.body);
+      req.session.userId = createdUser._id;
+      req.session.username = createdUser.username;
+      req.session.logged = true;
+      res.redirect('/dogs');
+      console.log(createdUser); // REMOVE BEFORE PRESENTATION
+    } catch(err) {
+      res.send(err);
+    }
   }
-
-
-
-
-
-
-
-
-
-
-
 }
 
 module.exports = authController;
