b99dc44bcc3bca69ad070487719e87b5af7daa8a
David Zinser <dmzinser@gmail.com>
2019-08-01 13:42:31 -0700

    change user model file name

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override')
 const session = require('express-session');
 const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT;
 
-require('dotenv').config();
 
 require ('./db/db');
 
