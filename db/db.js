f868d7b34cd24618e6e3a7496e2a08a4b1f063d6
David Zinser <dmzinser@gmail.com>
2019-08-01 13:35:23 -0700

    Deploy to Heroku

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-const connectionString = 'mongodb://localhost/men-stack-project';
+console.log(process.env.MONGODB_URI)
+const connectionString = process.env.MONGODB_URI;
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
