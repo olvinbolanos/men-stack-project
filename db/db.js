352030409b2cdd421c84499c2690a96fa8834437
Olvin <olvin.bolanos@yahoo.com>
2023-07-20 10:29:38 -0700

    changed mongo directory to mongodb_uri in db

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-console.log("mongodb://127.0.0.1:27017/db")
-const connectionString = "mongodb://127.0.0.1:27017/db";
+console.log(process.env.MONGODB_URI)
+const connectionString = process.env.MONGODB_URI;
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
